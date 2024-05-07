7f10aa7160c39f93bb4816ea24e6f68e93f5456d
less <ndeurys@gmail.com>
2024-05-07 05:36:13 +0000

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,121 @@
 import { Component } from '@angular/core';
+import { formatDistanceToNow } from 'date-fns';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'github-profil-finder';
+
+  baseUrl: string = 'https://api.github.com/users/';
+  username: string = 'lappranti';
+  profile: any = null;
+  findedProfile: any = null;
+  showFindedProfile: boolean = true;
+
+  repos!: any;
+  displRepos!: any;
+
+  infos: any[] = [];
+
+  constructor() {
+    this.getProfile();
+    this.getRepos();
+  }
+
+  getProfile() {
+    this.fetchProfile(this.username).then((data) => {
+      if (data.id) {
+        this.profile = data;
+        this.initiliseInfo(data);
+      }
+    });
+  }
+
+  getRepos() {
+    this.fetchRepos(this.username).then((data) => {
+      if (data) {
+        this.repos = data;
+        // this.displRepos = data.slice(0, 6);
+      }
+    });
+  }
+
+  formatDate(date: Date) {
+    return formatDistanceToNow(date, { addSuffix: true });
+  }
+
+  initiliseInfo(profile: any) {
+    this.infos = [];
+    this.infos.push({ label: 'Followers', value: profile.followers });
+    this.infos.push({ label: 'Following', value: profile.following });
+    this.infos.push({
+      label: 'Location',
+      value: profile.location,
+    });
+    this.infos.push({
+      label: 'Public repos',
+      value: profile.public_repos,
+    });
+  }
+
+  async fetchProfile(username: string) {
+    try {
+      const response = await fetch(this.baseUrl + username);
+      const data = await response.json();
+
+      console.log(data);
+
+      return data;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async fetchRepos(username: string) {
+    try {
+      const response = await fetch(`${this.baseUrl}${username}/repos`);
+      const data = await response.json();
+
+      console.log(data);
+
+      return data;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  handleViewAllRepos() {
+    this.displRepos = this.repos;
+  }
+
+  handleShowLessRepos() {
+    this.displRepos = this.repos.slice(0, 6);
+  }
+
+  search() {
+    if (this.username.length != 0) {
+      this.fetchProfile(this.username).then((data) => {
+        if (data.id) {
+          this.toggleProfileContainer();
+          this.findedProfile = data;
+        }
+      });
+    } else {
+      this.findedProfile = null;
+    }
+  }
+
+  selectProfile() {
+    this.profile = this.findedProfile;
+    this.getProfile();
+    this.getRepos();
+    this.toggleProfileContainer();
+  }
+
+  toggleProfileContainer() {
+    this.showFindedProfile = !this.showFindedProfile;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatIconModule,
+    RouterModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
